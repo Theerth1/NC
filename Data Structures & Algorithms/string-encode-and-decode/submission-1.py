254d95b7e7c62c317f9cfef6a734f129545325ce
Theerth Vikas Srinivasan <theerth.srinivasan@gmail.com>
2026-07-13 20:33:16 -0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,48 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+       strLength = "," + str(len(strs))
+       key = ""
+       encoded = ""
+       for s in strs:
+           key += f"{len(s)};"
+           encoded += s
+       key = key[:-1] + ","
+
+       encoded = key + encoded + strLength
+       return encoded
+
+    def decode(self, s: str) -> List[str]:
+        store = 0
+        for i in range(len(s)):
+            if s[i] == ",":
+                store = i
+        
+        length = int(s[store + 1:])
+        decoded = []
+        s = s[:store]
+
+        firstComma = 0
+        for i in range(len(s)):
+            if s[i] == ",":
+                firstComma = i
+                break
+
+        key = s[:firstComma]
+        s = s[firstComma + 1:]
+
+    
+        individualLengths = []
+        key = key.split(";")
+
+        for i in range(length):
+            k = int(key[i])
+            decoded.append(s[:k])
+            s = s[k:]
+        
+        return decoded
+
+
+
+
+

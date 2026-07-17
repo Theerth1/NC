97b111f70a659b0b1fcd2286d0ab4ff8814488c4
Theerth Vikas Srinivasan <theerth.srinivasan@gmail.com>
2026-07-17 16:46:41 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,36 @@
+import string
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean = ""
+        for char in s:
+            if char.isdigit():
+                clean += (char)
+            elif char in string.ascii_letters:
+                clean += (char.lower())
+        l = 0
+        r = len(clean) - 1
+
+        #even vs odd
+        # if even: "abab" if l = r -1 then stop
+        # if odd: "ababa" if l = r -2 then stop
+        if not clean:
+            return True
+
+        if len(clean) % 2 == 0:
+            while not (l > r):
+                if clean[l] == clean[r]:
+                    l += 1
+                    r -= 1
+                else:
+                    return False
+            return True
+        else:
+            while not (l == r):
+                if clean[l] == clean[r]:
+                    l += 1
+                    r -= 1
+                else:
+                    return False
+            return True
+        
